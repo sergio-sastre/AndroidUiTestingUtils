2139890628b5749453840e40a730268ba2e29b24
Sergio Sastre Flórez <sergio.sastre.florez@gmail.com>
2024-01-20 10:46:07 +0100

    Ensure it works on API 29 and update UiModeTestRule doc

--- utils/src/main/java/sergio/sastre/uitesting/utils/testrules/uiMode/UiModeTestRule.kt
+++ utils/src/main/java/sergio/sastre/uitesting/utils/testrules/uiMode/UiModeTestRule.kt
@@ -9,7 +9,7 @@ import sergio.sastre.uitesting.utils.common.UiMode
 /**
  * A TestRule to change the UiMode to day or night.
  *
- * It is strongly based on Adevinta/Barista DayNightRule.
+ * It is set via Adb from API 30+. For older APIs, it uses AppCompatDelegate, based on Adevinta/Barista DayNightRule.
  * https://github.com/AdevintaSpain/Barista/tree/master/library%2Fsrc%2Fmain%2Fjava%2Fcom%2Fadevinta%2Fandroid%2Fbarista%2Frule%2Ftheme
  *
  * WARNING: It's not compatible with Robolectric
@@ -20,7 +20,7 @@ class UiModeTestRule(private val mode: UiMode) : TestRule {
         return object : Statement() {
             @Throws(Throwable::class)
             override fun evaluate() {
-                when (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                when (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
                     true -> AdbUiModeSetter(base, description)
                     false -> AppCompatDelegateUiModeSetter(base, description)
                 }.setUiModeDuringTestOnly(mode)
