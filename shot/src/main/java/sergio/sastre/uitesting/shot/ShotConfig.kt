161f440b1c744f416cab82eb169455da86cebf83
Sergio Sastre Flórez <sergio.sastre.florez@gmail.com>
2024-03-07 00:03:57 +0100

    Add viewMaxPixels to ShotConfig, supported from Shot 6.1.0

--- shot/src/main/java/sergio/sastre/uitesting/shot/ShotConfig.kt
+++ shot/src/main/java/sergio/sastre/uitesting/shot/ShotConfig.kt
@@ -4,9 +4,16 @@ import androidx.annotation.ColorInt
 import sergio.sastre.uitesting.utils.crosslibrary.config.BitmapCaptureMethod
 import sergio.sastre.uitesting.utils.crosslibrary.config.LibraryConfig
 
+/**
+ * @param viewMaxPixels: Max Pixels for the generated screenshot before throwing an exception.
+ * WARNING:
+ *    1. Requires bitmapCaptureMethod = null
+ *    2. Works only with Views, not with Composables
+ */
 class ShotConfig(
     val ignoredViews: List<Int> = emptyList(),
     val prepareUIForScreenshot: () -> Unit = {},
     val bitmapCaptureMethod: BitmapCaptureMethod? = null,
     @ColorInt val backgroundColor: Int? = null,
+    val viewMaxPixels: Long = 10_000_000L,
 ): LibraryConfig
