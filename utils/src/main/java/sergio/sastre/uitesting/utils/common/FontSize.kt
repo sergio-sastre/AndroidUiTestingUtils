9d19d6f69438b048c8f0d9ea07b6a3477e9632d2
Sergio Sastre Flórez <sergio.sastre.florez@gmail.com>
2024-03-10 09:52:59 +0100

    Remove FontSizeLinear and FontSizeNonLinear values

--- utils/src/main/java/sergio/sastre/uitesting/utils/common/FontSize.kt
+++ utils/src/main/java/sergio/sastre/uitesting/utils/common/FontSize.kt
@@ -10,8 +10,21 @@ interface FontSizeScale {
     companion object {
         fun supportedValuesForCurrentSdk(): Array<FontSizeScale> =
             when (SDK_INT >= UPSIDE_DOWN_CAKE) {
-                true -> FontSizeNonLinear.entries.toTypedArray()
-                false -> FontSizeLinear.entries.toTypedArray()
+                true -> arrayOf(
+                    FontSize.SMALL,
+                    FontSize.NORMAL,
+                    FontSize.LARGE,
+                    FontSize.XLARGE,
+                    FontSize.XXLARGE,
+                    FontSize.XXXLARGE,
+                    FontSize.LARGEST
+                )
+                false -> arrayOf(
+                    FontSize.SMALL,
+                    FontSize.NORMAL,
+                    FontSize.LARGE,
+                    FontSize.LARGEST
+                )
             }
     }
 
@@ -30,7 +43,7 @@ private const val MAX_LINEAR_FONT_SCALE = 1.3f
 private const val MAX_NON_LINEAR_FONT_SCALE = 2.0f
 
 enum class FontSize(override val scale: Float) : FontSizeScale {
-    @Discouraged("Use XLARGE or MAXIMUM_LINEAR instead")
+    @Discouraged("Use LARGEST instead. Alternatively, also XLARGE or MAXIMUM_LINEAR")
     HUGE(MAX_LINEAR_FONT_SCALE),
 
     SMALL(0.85f),
@@ -47,26 +60,6 @@ enum class FontSize(override val scale: Float) : FontSizeScale {
     val value: String = scale.toString()
 }
 
-enum class FontSizeLinear(override val scale: Float) : FontSizeScale {
-    SMALL(0.85f),
-    NORMAL(1f),
-    LARGE(1.15f),
-    LARGEST(MAX_LINEAR_FONT_SCALE);
-
-    val value: String = scale.toString()
-}
-
-enum class FontSizeNonLinear(override val scale: Float) : FontSizeScale {
-    SMALL(0.85f),
-    NORMAL(1f),
-    LARGE(1.15f),
-    XLARGE(1.3f),
-    XXLARGE(1.5f),
-    XXXLARGE(1.8f),
-    LARGEST(MAX_NON_LINEAR_FONT_SCALE);
-
-    val value: String = scale.toString()
-}
 
 private val maxFontSizeScaleSupported =
     when (SDK_INT >= UPSIDE_DOWN_CAKE) {
