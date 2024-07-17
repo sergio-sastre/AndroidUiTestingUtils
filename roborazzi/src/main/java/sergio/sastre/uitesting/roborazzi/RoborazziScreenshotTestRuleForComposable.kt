94d938b1440d3b68b7433b0356ba5e1bdf09c140
Sergio Sastre Flórez <sergio.sastre.florez@gmail.com>
2024-07-17 21:51:12 +0200

    Avoid crash in ComposableTests if RoborazziConfig captureType is DUMP

--- roborazzi/src/main/java/sergio/sastre/uitesting/roborazzi/RoborazziScreenshotTestRuleForComposable.kt
+++ roborazzi/src/main/java/sergio/sastre/uitesting/roborazzi/RoborazziScreenshotTestRuleForComposable.kt
@@ -1,5 +1,6 @@
 package sergio.sastre.uitesting.roborazzi
 
+import android.view.View
 import androidx.compose.runtime.Composable
 import com.github.takahirom.roborazzi.ExperimentalRoborazziApi
 import com.github.takahirom.roborazzi.captureRoboImage
@@ -8,6 +9,7 @@ import org.junit.runners.model.Statement
 import sergio.sastre.uitesting.robolectric.activityscenario.RobolectricActivityScenarioForComposableRule
 import sergio.sastre.uitesting.roborazzi.config.RoborazziSharedTestAdapter
 import sergio.sastre.uitesting.mapper.roborazzi.RoborazziConfig
+import sergio.sastre.uitesting.mapper.roborazzi.wrapper.CaptureType
 import sergio.sastre.uitesting.utils.crosslibrary.config.LibraryConfig
 import sergio.sastre.uitesting.utils.crosslibrary.config.ScreenshotConfigForComposable
 import sergio.sastre.uitesting.utils.crosslibrary.testrules.ScreenshotTestRuleForComposable
@@ -39,17 +41,30 @@ class RoborazziScreenshotTestRuleForComposable(
         snapshot(null, composable)
     }
 
-
     override fun snapshot(name: String?, composable: @Composable () -> Unit) {
-        @OptIn(ExperimentalRoborazziApi::class)
-        activityScenarioRule
+        val view = activityScenarioRule
             .setContent { composable() }
             .composeView
-            .drawToBitmap(roborazziConfig.bitmapCaptureMethod)
-            .captureRoboImage(
-                filePath = filePathGenerator(roborazziConfig.filePath, name),
-                roborazziOptions = roborazziAdapter.asRoborazziOptions(),
-            )
+        snapshotView(name, view)
+    }
+
+    private fun snapshotView(name: String?, view: View) {
+        @OptIn(ExperimentalRoborazziApi::class)
+        when (roborazziConfig.roborazziOptions.captureType is CaptureType.Dump) {
+            // Dump does not support Bitmap.captureRoboImage
+            true -> view
+                .captureRoboImage(
+                    filePath = filePathGenerator(roborazziConfig.filePath, name),
+                    roborazziOptions = roborazziAdapter.asRoborazziOptions(),
+                )
+
+            false -> view
+                .drawToBitmap(roborazziConfig.bitmapCaptureMethod)
+                .captureRoboImage(
+                    filePath = filePathGenerator.invoke(roborazziConfig.filePath, name),
+                    roborazziOptions = roborazziAdapter.asRoborazziOptions(),
+                )
+        }
     }
 
     override fun configure(config: LibraryConfig): ScreenshotTestRuleForComposable = apply {
