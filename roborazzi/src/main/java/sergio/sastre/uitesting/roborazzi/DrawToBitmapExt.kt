7a0cda1b6dc4ea3ab5e93b918f13b37bd19b1a1d
Sergio Sastre Flórez <sergio.sastre.florez@gmail.com>
2024-07-17 21:17:40 +0200

    Ensure bitmapCaptureMode is applied only when Roborazzi allows it i.e. captureType != DUMP

--- roborazzi/src/main/java/sergio/sastre/uitesting/roborazzi/DrawToBitmapExt.kt
+++ roborazzi/src/main/java/sergio/sastre/uitesting/roborazzi/DrawToBitmapExt.kt
@@ -7,12 +7,16 @@ import androidx.core.view.drawToBitmap
 import sergio.sastre.uitesting.utils.crosslibrary.config.BitmapCaptureMethod
 import sergio.sastre.uitesting.utils.utils.drawToBitmapWithElevation
 
-internal fun View.drawToBitmap(bitmapCaptureMethod: BitmapCaptureMethod?): Bitmap =
+internal fun View.drawToBitmap(
+    bitmapCaptureMethod: BitmapCaptureMethod?
+): Bitmap =
     when (bitmapCaptureMethod){
         is BitmapCaptureMethod.Canvas -> drawToBitmap(config = bitmapCaptureMethod.config)
         is BitmapCaptureMethod.PixelCopy -> drawToBitmapWithElevation(config = bitmapCaptureMethod.config)
         null -> drawToBitmapWithElevation()
     }
 
-internal fun Dialog.drawToBitmap(bitmapCaptureMethod: BitmapCaptureMethod?): Bitmap =
+internal fun Dialog.drawToBitmap(
+    bitmapCaptureMethod: BitmapCaptureMethod?,
+): Bitmap =
     window!!.decorView.drawToBitmap(bitmapCaptureMethod)
