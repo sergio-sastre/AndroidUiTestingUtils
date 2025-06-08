713f45c397701ca0bb83a6ece2ec4fc68761329d
Sergio Sastre Flórez <sergio.sastre.florez@gmail.com>
2025-06-08 23:20:05 +0200

    Add HighTextContrastTestRule and FontWeightTestRule

---
+++ utils/src/main/java/sergio/sastre/uitesting/utils/testrules/accessibility/HighTextContrastTestRule.kt
@@ -0,0 +1,38 @@
+package sergio.sastre.uitesting.utils.testrules.accessibility
+
+import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
+import org.junit.rules.TestRule
+import org.junit.runner.Description
+import org.junit.runners.model.Statement
+import sergio.sastre.uitesting.utils.utils.waitForExecuteShellCommand
+import java.io.IOException
+
+/**
+ * Allows to enable/disable high text contrast accessibility during a test.
+ *
+ * WARNING: Only works on Instrumentation tests, not on Robolectric tests.
+ */
+class HighTextContrastTestRule: TestRule {
+    override fun apply(base: Statement, description: Description): Statement {
+        return object : Statement() {
+            @Throws(Throwable::class)
+            override fun evaluate() {
+                try {
+                    enableHighTextContrast(true)
+                    base.evaluate()
+                } finally {
+                    enableHighTextContrast(false)
+                }
+            }
+        }
+    }
+
+    @Throws(IOException::class)
+    private fun enableHighTextContrast(enable: Boolean = true) {
+        val animationEnabledValue = when (enable) {
+            true -> 1
+            false -> 0
+        }
+        getInstrumentation().waitForExecuteShellCommand("settings put secure high_text_contrast_enabled $animationEnabledValue")
+    }
+}
